1c5f8bbce27204cabe72a2b12a4943944dc86f2b
ShahariarNiloy <shahariarniloyblv@gmail.com>
2023-06-22 18:48:29 +0600

    Search modal added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import LoginModal from "@/components/modals/LoginModal";
 import getCurrentUser from "@/actions/getCurrentUser";
 import ToasterProvider from "@/providers/ToasterProvider";
 import RentModal from "@/components/modals/RentModal";
+import SearchModal from "@/components/modals/SearchModal";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className} suppressHydrationWarning={true}>
         <ToasterProvider />
+        <SearchModal />
         <RentModal />
         <RegisterModal />
         <LoginModal />
--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -12,7 +12,7 @@ const Logo = () => {
       className="hidden md:block cursor-pointer"
       src="/images/logo.png"
       height="50"
-      width="100"
+      width="150"
       alt="Logo"
       loading="lazy"
       style={{ objectFit: "contain" }}
